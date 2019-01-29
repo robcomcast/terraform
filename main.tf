a37c7928b767e189d834cc7ee19f84cc2eae2373
Robert Barrimond <robert@barrimond.com>
2019-01-28 20:45:35 -0500

    created ASG

--- main.tf
+++ main.tf
@@ -7,24 +7,16 @@ variable "server_port" {
   default = 8080
 }
 
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
-}
-
 resource "aws_launch_configuration" "example" {
-    ami                     =   "ami-40d28157"
-    instance_type           =   "t2.micro"
-    vpc_security_group_ids  =   [ "${aws_security_group.instance.id}" ]
+    image_id                     =   "ami-40d28157"
+    instance_type                =   "t2.micro"
+    security_groups              =   [ "${aws_security_group.instance.id}" ]
 
     user_data       = <<-EOF
                      #!/bin/sh
                      echo "Hello, World!" > index.html
                      nohup busybox httpd -f -p "${var.server_port}" &
                      EOF
-
-    tags {
-        Name        = "terraform-example"
-    }
 
     lifecycle {
         create_before_destroy = true
@@ -48,8 +40,11 @@ resource "aws_security_group" "instance" {
 
 resource "aws_autoscaling_group" "example" {
 
-    launch_configuration = "${aws_launch_configuration.example}"
-    availability_zones   = "${data.aws_availability_zones.all.names}"
+    launch_configuration    = "${aws_launch_configuration.example.id}"
+    availability_zones      = ["${data.aws_availability_zones.all.names}"]
+
+    load_balancers          = ["${aws_elb.example.name}"]
+    health_check_type       = "ELB"
 
     min_size = 2
     max_size = 10
@@ -64,4 +59,47 @@ resource "aws_autoscaling_group" "example" {
 
 data "aws_availability_zones" "all" {
   
+}
+
+resource "aws_elb" "example" {
+    name                = "terraform-asg-example"
+    availability_zones  = ["${data.aws_availability_zones.all.names}"]
+    security_groups     = [ "${aws_security_group.elb.id}" ]
+
+    listener {
+        lb_port             = 80
+        lb_protocol         = "http"
+        instance_port       = "${var.server_port}"
+        instance_protocol   = "http"
+    }
+
+    health_check {
+        healthy_threshold   = 2
+        unhealthy_threshold = 2
+        timeout             = 3
+        interval            = 30
+        target              = "HTTP:${var.server_port}/"
+    }
+}
+
+resource "aws_security_group" "elb" {
+
+    ingress {
+        from_port       = 80
+        to_port         = 80
+        protocol        = "tcp"
+        cidr_blocks     = [ "0.0.0.0/0" ]
+    }
+
+    egress {
+        from_port       = 80
+        to_port         = 80
+        protocol        = "-1"
+        cidr_blocks     = [ "0.0.0.0/0" ]
+    }
+  
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.name}"
 }
